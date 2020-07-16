14a74429e4ea8831947a9d4f86c24eddf244679f
prashun <prashunkumar039@gmail.com>
2020-07-16 23:00:38 +0530

    Added file empWageComputation to Check Employee is present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,32 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=5;
-empRatePerHr=20;
-numWorkingDays=20;
-declare -A totalSalary
-
-for (( day=1; day<=$numWorkingDays; day++ ))
-do
-    empCheck=$((RANDOM%3));
-         case $empCheck in
-                 $isFullTime)
-                       empHrs=8
-                            ;;
-                 $isPartTime)
-                        empHrs=4
-                            ;;
-               *)
-               empHrs=0
-                           ;;
-esac
-
-    salary=$(($empHrs*$empRatePerHr));
-    totalSalary=$(($salary+${totalSalary[@]}));
-
-
-done
-
-echo ${totalSalary[@]}
-echo ${!totalSalary[@]}
+var=`echo $(( Random % 2))`
+if [ $var -eq 1 ]
+then
+      echo 'employ is present'
+else
+      echo 'employ is not present'
+fi 
