432b6a312e4fc4491c5a575f6f1112aaf3dbea41
prashun <prashunkumar039@gmail.com>
2020-07-17 12:41:35 +0530

    Stores the daily wage along with the total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,47 @@
+  GNU nano 4.9.3                                                                                 uc8
 #!/bin/bash -x
+isPartTime=1
+isFullTime=2
+MAX_HRS_IN_MONTH=100
+empRatePerHr=20
+numWorkingDays=20
 
-var=`echo $(( Random % 2))`
-if [ $var -eq 1 ]
-then
-      echo 'employ is present'
-else
-      echo 'employ is not present'
-fi 
+totalEmpHr=0
+totalWorkingDays=0
+
+function getWorkingHrs() {
+
+     case $i in
+        $isFullTime)
+         workHrs=8
+            ;;
+
+        $isPartTime)
+           workHrs=4
+             ;;
+
+        *)
+          workHrs=0
+echo $workHrs                   ;;
+esac
+}
+
+function empDailyWage () {
+    local workHrs=$1
+    wages=$(($workHrs*$EMP_RATE_PER_HR))
+    echo $wage
+}
+while [[ $totalWorkHr -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $numWorkingDays ]]
+do
+   ((totalWorkingDays++))
+   workHrs="$( getWorkingHrs $((RANDOM%3)) )"
+   totalWorkHrs=$(($totalworkHrs+$workHrs))
+   dailyWage[$totalWorkingDays=$(($empHrs*$EMP_RATE_PER_HR))
+
+
+
+done
+
+totalSalary=$(($totalWorkHrs*$empRatePerHr))
+echo ${dailyWageDailyWage[@]}
