c48790c31eba2a8afa3c0886b8cd9e47095df7a8
prashun <prashunkumar039@gmail.com>
2020-07-17 11:17:25 +0530

    Calculate wages till a condition of total working hrs or days is reached for a mth

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,37 @@
 #!/bin/bash -x
+isPartTime=1
+isFullTime=2
+MAX_HRS_IN_MONTH=100
+empRatePerHr=20
+numWorkingDays=20
 
-var=`echo $(( Random % 2))`
-if [ $var -eq 1 ]
-then
-      echo 'employ is present'
-else
-      echo 'employ is not present'
-fi 
+totalEmpHr=0
+totalWorkingDays=0
+
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $numWorkingDays ]]
+do
+   ((totalWorkingDays++))
+   empcheck=$((RANDOM%3))
+   case $empcheck in
+        $isFullTime)
+         empHrs=8
+            ;;
+
+        $isPartTime)
+           empHrs=4
+             ;;
+
+        *)
+          empHrs=0
+                   ;;
+esac
+
+
+totalEmpHr=$(($totalEmpHr+$empHrs))
+
+done
+
+totalSalary=$(($totalEmpHr*$empRatePerHr))
+
+
