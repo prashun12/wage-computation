8f5e9bb8cacdf3db383b49f8827f00c2ad74d4a4
prashun <prashunkumar039@gmail.com>
2020-07-17 11:42:34 +0530

    Refactor the code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,43 @@
 #!/bin/bash -x
+isPartTime=1
+isFullTime=2
+MAX_HRS_IN_MONTH=100
+empRatePerHr=20
+numWorkingDays=20
 
-var=`echo $(( Random % 2))`
-if [ $var -eq 1 ]
-then
-      echo 'employ is present'
-else
-      echo 'employ is not present'
-fi 
+totalEmpHr=0
+totalWorkingDays=0
+
+function getWorkingHrs() {
+
+     case $i in
+        $isFullTime)
+         workHrs=8
+            ;;
+
+        $isPartTime)
+           workHrs=4
+             ;;
+
+        *)
+          workHrs=0
+echo $workHrs                   ;;
+esac
+}
+
+
+while [[ $totalWorkHr -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $numWorkingDays ]]
+do
+   ((totalWorkingDays++))
+   workHrs="$( getWorkingHrs $((RANDOM%3)) )"
+   totalWorkHrs=$(($totalworkHrs+$workHrs))
+
+
+
+
+done
+
+totalSalary=$(($totalWorkHrs*$empRatePerHr))
+
+
