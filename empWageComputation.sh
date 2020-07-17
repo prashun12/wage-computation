2ba0ae1b0778884633f44e77b4b160bcaf7cfcca
prashun <prashunkumar039@gmail.com>
2020-07-17 20:41:01 +0530

    day11 question

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,27 @@
 #!/bin/bash -x
 
-var=`echo $(( Random % 2))`
-if [ $var -eq 1 ]
+# take three integers from the user
+echo "Enter three integers: "
+read a b c
+decalre -A cal
+cal[a+b*c]= "(($a+$b\*$c))"
+cal[a*b+c]= "(($a%$b +$c))"
+cal[c+a/b]= "(($c+$a/ $c))"
+cal[a%b+c] ="(($a \*$b +$c))"
+echo ${!cal[@]}
+echo ${cal[@]}
+counter=0
+for value in ${cal[@]}
+do
+    cal[((counter++))]
+done
+for((i=0; i<3; i++))
+do
+   for((j=$(( $i+1 ));j<4;j++))
+do
+if [ ${cal[$j]} -gt ${arr[$i]}
 then
-      echo 'employ is present'
-else
-      echo 'employ is not present'
-fi 
+temp=${cal[$j]}
+cal[$i]=${cal[$]}
+cal[$]=$temp
+fi
